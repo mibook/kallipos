1cbc734c56d6bdf67af47e289627ab66ddea22c5
epidrome <github@epidro.me>
2024-08-10 12:24:12 +0200

    updated footnote image tag

--- footnote.lua
+++ footnote.lua
@@ -11,7 +11,7 @@ function Note(el)
         f:close()
         local title=pandoc.utils.stringify(doc.meta.title) or "Title has not been set"
         local license_text=pandoc.utils.stringify(doc.meta.license_text) or "License has not been set"
-        return pandoc.Note("*@" .. ft .. " " .. title .. " (" .. license_text .. ")")
+        return pandoc.Note("@" .. ft .. " " .. title .. " (" .. license_text .. ")")
     end
   end
 end
